1766558ca019792d662073b6408c6b3fa546aa2a
Andrii Stafiniak <andrii.stafiniak@gmail.com>
2023-08-18 10:22:16 +0200

    adding user interaction

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
-// import Form from "./Form"
-import { useState } from "react"
+import Form from "./Form"
+
+
 
 
 function App() {
 
-  const [name, setName] = useState("");
-  const onSubmitForm = (event) => {
-    event.preventDefault();
-    console.log(name);
-    setName("");
-  }
+
+
+
+
+
+
+
+
+
   return (
-    <form onSubmit={onSubmitForm}>
-      <input
-        value={name}
-        onChange={(event) => setName(event.target.value)} />
-
-      <button>Wyslij</button>
-      <p>{name}</p>
-
-    </form>
-
-    // <Form />
+    <>
+      <Form />
+    </>
   );
 }
 
--- src/Form/index.js
+++ src/Form/index.js
@@ -1,23 +1,78 @@
-import "./style.css"
-const Form = (props) => (<form className="form">
-   <fieldset className="form__fieldset">
-      <legend className="form__legend">Currency converter</legend>
-      <label className="form__label">
-         Wpisz kwote w PLN:
-         <input className=" form__input" type="number" name="euro" min="1" step="any" placeholder="Kwota w PLN"
-            required />
-      </label>
-      <label>
-         Wybierz walute:
-         <select name="currency " className="form__select ">
-            <option className="Euro" value="EUR">Euro €</option>
-            <option className="Dolar" value="USD">Dolar $</option>
-         </select>
-      </label>
-      <button className="form__button ">Oblicz</button>
-      <p className="form__paragraph">Wynik:<span className="form__span">n/a</span></p>
-   </fieldset>
-</form>
-)
+import "./style.css";
+import { useState } from "react";
+
+const Form = () => {
+   const [amount, setAmount] = useState("");
+   const [currency, setCurrency] = useState("EUR");
+   const [result, setResult] = useState(null);
+
+   const currentExchangeRates = {
+      EUR: 4.62,
+      USD: 4.21,
+      GBT: 5.21,
+   };
+   const onCurrencyChange = (event) => {
+      setCurrency(event.target.value);
+      setResult(null);
+   };
+   const onAmountChange = (event) => {
+      setAmount(event.target.value);
+      setResult(null);
+   };
+
+   const calculateResult = (currency, amount) => {
+      return amount / currentExchangeRates[currency];
+   };
+
+   const updateResultText = () => {
+      if (result != null) {
+         return <p>{amount} {"PLN = "} {result.toFixed(2)} {currency}</p>;
+      }
+      return null;
+   };
+
+   const onFormSubmit = (event) => {
+      event.preventDefault();
+      let calculatedResult = calculateResult(currency, amount);
+      setResult(calculatedResult);
+   };
+
+   return (
+      <form onSubmit={onFormSubmit} className="form">
+         <fieldset className="form__fieldset">
+            <legend className="form__legend">Currency converter</legend>
+            <label className="form__label">
+               Wpisz kwotę w PLN:
+               <input
+                  value={amount}
+                  onChange={onAmountChange}
+                  className="form__input"
+                  type="number"
+                  name="euro"
+                  min="1"
+                  step="any"
+                  placeholder="Kwota w PLN"
+                  required />
+            </label>
+            <label>
+               Wybierz walutę:
+               <select
+                  className="js-currency"
+                  value={currency}
+                  onChange={onCurrencyChange}
+               >
+                  <option value="GBT">GBT</option>
+                  <option value="EUR">EUR</option>
+                  <option value="USD">USD</option>
+               </select>
+            </label>
+            <button
+               type="submit"
+               className="form__button">Oblicz</button>
+            {updateResultText()}
+         </fieldset>
+      </form>
+   );
+};
 
 export default Form;
