85f8d27a9911af3d3f030f0f66eb701b5367f38b
Andrii Stafiniak <andrii.stafiniak@gmail.com>
2023-10-12 00:45:44 +0200

    remove server

--- src/Form/useFetchData.js
+++ src/Form/useFetchData.js
@@ -1,38 +1,40 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react'
 
-const useFetchData = (apiUrl) => {
-   const [data, setData] = useState(null);
-   const [loading, setLoading] = useState(true);
-   const [error, setError] = useState(null);
-   const url = '/api/latest';
-
+export const useFetchData = () => {
+   const [ratesData, setRatesData] = useState({
+      state: "loading",
+      data: null,
+   });
 
    useEffect(() => {
       const fetchData = async () => {
          try {
-            const response = await fetch(`${apiUrl}`);
+            const response = await fetch(`https://api.currencyapi.com/v3/latest?apikey=cur_live_W4XgneKvCr75wX0qJDc8IGwz5j5Eg4wstpdf6tDQ`);
+
             if (!response.ok) {
                throw new Error(response.statusText);
             }
-            const result = await response.json();
-            setData(result);
+
+            const data = await response.json();
+
+            console.log('Pobrane dane:', data);
+
+            setRatesData({
+               state: "success",
+               data,
+            });
+
          } catch (error) {
-            setError(error);
-         } finally {
-            setLoading(false);
+            console.error('Error fetching data:', error);
+            setRatesData({
+               state: "error",
+               data: null,
+            });
          }
       };
 
-      const timeout = setTimeout(() => {
-         fetchData();
-      }, 2000);
+      setTimeout(fetchData, 2000);
+   }, []);
 
-      return () => clearTimeout(timeout);
-   }, [apiUrl]);
-
-
-   return { data, loading, error };
+   return { ratesData };
 };
-
-export default useFetchData;
-
